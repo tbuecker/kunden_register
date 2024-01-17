e07d6d5cc5d88f74f9955059fbdfcca197b6b8aa
Thomas Bücker <155522890+TomB86@users.noreply.github.com>
2024-01-17 13:44:45 +0100

    Suche hinzu gefügt

--- suchen.py
+++ suchen.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import subprocess
+import json
 
 
 def back_button_action():
@@ -8,7 +9,27 @@ def back_button_action():
     subprocess.run(["python", start_path])
 
 def suchen_button_action():
-    pass
+    vorname_input = kunden_vornme_eingabe.get()
+    nachname_input = kunden_nachname_eingabe.get()
+    geburtsdatum_input = kunden_geburtsdatum_eingabe.get()
+    mail_input = kunden_mail_eingabe.get()
+    ort_input = kunden_ort_eingabe.get()
+
+    with open("Kundendaten.json", "r", encoding="utf-8") as file:
+        for line in file:
+            customer_data = json.loads(line)
+
+            # Prüfen Sie, ob die eingegebenen Suchkriterien übereinstimmen
+            if (
+                (not vorname_input or customer_data.get("vorname") == vorname_input) and
+                (not nachname_input or customer_data.get("nachname") == nachname_input) and
+                (not geburtsdatum_input or customer_data.get("geburtsdatum") == geburtsdatum_input) and
+                (not mail_input or customer_data.get("email") == mail_input) and
+                (not ort_input or customer_data.get("wohnort") == ort_input)
+            ):
+                # Hier werden die gefundenen Kundendaten ausgegeben
+                print(f"Gefundener Kunde: {customer_data}")
+
 
 suchfenster = Tk()
 suchfenster.geometry("500x260")
