9f45b8c05d8adda6ca88cadb93852b4765e837c4
Thomas Bücker <155522890+TomB86@users.noreply.github.com>
2024-01-18 12:05:29 +0100

    Version 2.3

---
+++ ausgabefenster.py
@@ -0,0 +1,64 @@
+import json
+from tkinter import *
+import subprocess
+import os
+
+def back_button_action():
+    ausgabefenster.destroy()
+    #os.remove("searchassist.json")
+    start_path = "./suchen.py"
+    subprocess.run(["python", start_path])
+
+def change_button_action():
+    ausgabefenster.destroy()
+    start_path = "./changeingfenster.py"
+    subprocess.run(["python", start_path])
+
+with open("searchassist.json", "r", encoding="utf-8") as reader:
+    ausgabedaten = json.load(reader)
+
+
+ausgabefenster = Tk()
+ausgabefenster.geometry("400x330")
+ausgabefenster.title("Kunden Ausgabe")
+
+kunden_vornme_label = Label(ausgabefenster,text="Vorname : ", anchor="w", justify=LEFT)
+kunden_vornme_label.place(x=20, y=20, width= 360 , height= 20)
+kunden_vornme_ausgabe_label = Label(ausgabefenster,text=ausgabedaten["vorname"], anchor="w", justify=LEFT)
+kunden_vornme_ausgabe_label.place(x=140, y=20, width= 360 , height= 20)
+
+kunden_nachname_label = Label(ausgabefenster,text="Nachname : ", anchor="w", justify=LEFT)
+kunden_nachname_label.place(x=20, y=60, width= 100 , height= 20)
+kunden_nachname_ausgabe_label = Label(ausgabefenster,text=ausgabedaten["nachname"], anchor="w", justify=LEFT)
+kunden_nachname_ausgabe_label.place(x=140, y=60, width= 360 , height= 20)
+
+kunden_geburtsdatum_label = Label(ausgabefenster,text="Geburtsdatum : ", anchor="w", justify=LEFT)
+kunden_geburtsdatum_label.place(x=20, y=100, width= 100 , height= 20)
+kunden_geburtsdatum_ausgabe_label = Label(ausgabefenster,text=ausgabedaten["geburtsdatum"], anchor="w", justify=LEFT)
+kunden_geburtsdatum_ausgabe_label.place(x=140, y=100, width= 360 , height= 20)
+
+kunden_mail_label = Label(ausgabefenster,text="E-Mail : ", anchor="w", justify=LEFT)
+kunden_mail_label.place(x=20, y=140, width= 100 , height= 20)
+kunden_mail_ausgabe_label = Label(ausgabefenster,text=ausgabedaten["email"], anchor="w", justify=LEFT)
+kunden_mail_ausgabe_label.place(x=140, y=140, width= 360 , height= 20)
+
+kunden_ort_label = Label(ausgabefenster,text="Wohnort : ", anchor="w", justify=LEFT)
+kunden_ort_label.place(x=20, y=180, width= 100 , height= 20)
+kunden_ort_ausgabe_label = Label(ausgabefenster,text=ausgabedaten["wohnort"], anchor="w", justify=LEFT)
+kunden_ort_ausgabe_label.place(x=140, y=180, width= 360 , height= 20)
+
+kunden_id_label = Label(ausgabefenster,text="Kundennummer : ", anchor="w", justify=LEFT)
+kunden_id_label.place(x=20, y=220, width= 100 , height= 20)
+kunden_id_ausgabe_label = Label(ausgabefenster,text=ausgabedaten["id"], anchor="w", justify=LEFT)
+kunden_id_ausgabe_label.place(x=140, y=220, width= 360 , height= 20)
+
+back_button = Button(ausgabefenster,text="<-Zurück", command=back_button_action)
+back_button.place(x=30, y=260, width= 70 , height= 20)
+
+change_button = Button(ausgabefenster,text="Bearbeiten", command=change_button_action)
+change_button.place(x=130, y=260, width= 100 , height= 20)
+
+exit_button = Button(ausgabefenster,text="Beenden", command=ausgabefenster.quit)
+exit_button.place(x=260, y=260, width= 100 , height= 20)
+
+ausgabefenster.mainloop()
--- suchen.py
+++ suchen.py
@@ -14,6 +14,14 @@ def suchen_button_action():
     geburtsdatum_input = kunden_geburtsdatum_eingabe.get()
     mail_input = kunden_mail_eingabe.get()
     ort_input = kunden_ort_eingabe.get()
+    id_input_str = kunden_id_eingabe.get()
+
+    # Überprüfen, ob die Eingabe für die Kunden-ID nicht leer ist
+    if id_input_str:
+        id_input = int(id_input_str)
+    else:
+        # Handle den Fall, wenn die Eingabe leer ist (z.B. setze id_input auf None)
+        id_input = None
 
     with open("Kundendaten.json", "r", encoding="utf-8") as file:
         for line in file:
@@ -25,45 +33,58 @@ def suchen_button_action():
                 (not nachname_input or customer_data.get("nachname") == nachname_input) and
                 (not geburtsdatum_input or customer_data.get("geburtsdatum") == geburtsdatum_input) and
                 (not mail_input or customer_data.get("email") == mail_input) and
-                (not ort_input or customer_data.get("wohnort") == ort_input)
+                (not ort_input or customer_data.get("wohnort") == ort_input) and
+                (id_input is None or customer_data.get("id") == id_input)
             ):
                 # Hier werden die gefundenen Kundendaten ausgegeben
-                print(type(f"Gefundener Kunde: {customer_data}"))
+                #print(type(f"Gefundener Kunde: {customer_data}"))
+                with open("searchassist.json", "w",encoding="utf-8") as writer:
+                    json.dump(customer_data, writer, ensure_ascii=False)
+
+                suchfenster.destroy()
+
+                ausgabe_path = "./ausgabefenster.py"
+                subprocess.run(["python", ausgabe_path])
 
 
 suchfenster = Tk()
-suchfenster.geometry("500x260")
+suchfenster.geometry("500x300")
 suchfenster.title("Kunden Suche")
 
-kunden_vornme_label = Label(suchfenster,text="Vorname : ")
+kunden_vornme_label = Label(suchfenster,text="Vorname : ", anchor="w", justify=LEFT)
 kunden_vornme_label.place(x=20, y=20, width= 100 , height= 20)
 kunden_vornme_eingabe=Entry(suchfenster, bd=2)
 kunden_vornme_eingabe.place(x=120, y=20, width=360, height= 20 )
 
-kunden_nachname_label = Label(suchfenster,text="Nachname : ")
+kunden_nachname_label = Label(suchfenster,text="Nachname : ", anchor="w", justify=LEFT)
 kunden_nachname_label.place(x=20, y=60, width= 100 , height= 20)
 kunden_nachname_eingabe=Entry(suchfenster, bd=2)
 kunden_nachname_eingabe.place(x=120, y=60, width=360, height= 20 )
 
-kunden_geburtsdatum_label = Label(suchfenster,text="Geburtsdatum : ")
+kunden_geburtsdatum_label = Label(suchfenster,text="Geburtsdatum : ", anchor="w", justify=LEFT)
 kunden_geburtsdatum_label.place(x=20, y=100, width= 100 , height= 20)
 kunden_geburtsdatum_eingabe=Entry(suchfenster, bd=2)
 kunden_geburtsdatum_eingabe.place(x=120, y=100, width=360, height= 20 )
 
-kunden_mail_label = Label(suchfenster,text="E-Mail : ")
+kunden_mail_label = Label(suchfenster,text="E-Mail : ", anchor="w", justify=LEFT)
 kunden_mail_label.place(x=20, y=140, width= 100 , height= 20)
 kunden_mail_eingabe=Entry(suchfenster, bd=2)
 kunden_mail_eingabe.place(x=120, y=140, width=360, height= 20 )
 
-kunden_ort_label = Label(suchfenster,text="Wohnort : ")
+kunden_ort_label = Label(suchfenster,text="Wohnort : ", anchor="w", justify=LEFT)
 kunden_ort_label.place(x=20, y=180, width= 100 , height= 20)
 kunden_ort_eingabe=Entry(suchfenster, bd=2)
 kunden_ort_eingabe.place(x=120, y=180, width=360, height= 20 )
 
+kunden_id_label = Label(suchfenster,text="Kunden ID : ", anchor="w", justify=LEFT)
+kunden_id_label.place(x=20, y=220, width= 100 , height= 20)
+kunden_id_eingabe=Entry(suchfenster, bd=2)
+kunden_id_eingabe.place(x=120, y=220, width=360, height= 20 )
+
 suchen_button = Button(suchfenster, text=" - Suchen - ", command=suchen_button_action)
-suchen_button.place(x=120,y=220,width=360,height=20)
+suchen_button.place(x=120,y=260,width=360,height=20)
 
 back_button = Button(suchfenster,text="<-Zurück", command=back_button_action)
-back_button.place(x=30, y=220, width= 70 , height= 20)
+back_button.place(x=30, y=260, width= 70 , height= 20)
 
 suchfenster.mainloop()
